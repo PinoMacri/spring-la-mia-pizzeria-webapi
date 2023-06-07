879ea3d44195adb8fa3363b7a9fce92ea7e39539
PinoMacri <pinonegozio1@outlook.it>
2023-06-07 17:36:20 +0200

    last fixed

--- src/main/java/com/example/demo/ApiPizzaController.java
+++ src/main/java/com/example/demo/ApiPizzaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -72,7 +73,7 @@ public class ApiPizzaController {
 	 */
 
 	// UPDATE http://localhost:8080/api/pizza/v1/pizze/update/199
-	@PostMapping("/pizze/update/{pizzaId}")
+	@PutMapping("/pizze/update/{pizzaId}")
 	public ResponseEntity<Pizza> updatePizza(@PathVariable int pizzaId,
 			@RequestParam(value = "ingredientiSelezionati", required = false) List<Integer> ingredientiSelezionati,
 			@RequestBody Pizza updatedPizza) {
